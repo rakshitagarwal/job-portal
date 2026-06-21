481d0918f2529f7f8908f5d40bb61127dbc04b2a
Rakshit Agarwal <rakshitag13@gmail.com>
2026-06-21 19:31:02 +0530

    fix: remove ignored files from git tracking

--- services/payment/dist/routes/payment.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { isAuth } from "../middlewares/auth.js";
-import { checkOut, paymentVerification } from "../controllers/payment.js";
-const router = express.Router();
-router.post("/checkout", isAuth, checkOut);
-router.post("/verify", isAuth, paymentVerification);
-export default router;
